84e22eaf47ece71cc97691025c55c78a1db9a2f3
rk794 <bobkohler34@gmail.com>
2025-06-06 08:57:02 -0400

    better pathing

--- HelperScripts/Pull_Matrices.R
+++ HelperScripts/Pull_Matrices.R
@@ -1,23 +1,30 @@
 library(tidyverse)
 
 # Paths to subject lists and matrix file 
-matrix_sublist_path <- '/Users/bobkohler/Desktop/ARS/ABCD_Projects/abcd_matrices_general/timepoint2/tp2_mid_sublist.csv' #Use if subject list not in matrix file 
-outcome_sublist_path    <- '/Users/bobkohler/Desktop/ABCD matrix/tp2_mid_sublist.csv' 
-matrices_path           <- '/Users/bobkohler/Desktop/ARS/ABCD_Projects/abcd_matrices_general/timepoint2/tp2_MID_FFD15_matrix.rds'
+matrix_sublist_path     <- '' #Use if subject list not in matrix file 
+outcome_sublist_path    <- '' 
+matrices_path           <- ''
 
 # Path and RDS file name for filtered matrices 
-new_rds_path_name <- '/Users/bobkohler/Desktop/ARS/ABCD_Projects/abcd_matrices_general/timepoint2/tp2_mid_alcohol_matrices.rds'
+filtered_matrix_path    <- ''
+filtered_outcome_path   <- ''
+
+filtered_matrix_filename  <- ""
+filtered_outcome_filename <- ""
+
+
 
 #Read in matrices and pull IDs 
 connectivity_matrices <- read_rds(matrices_path)
-goodmatrix_sublist <- data.frame(subject_id = names(connectivity_matrices))
+goodmatrix_sublist <- data.frame(src_subject_id = names(connectivity_matrices)) # make sure subject id variable name matches subject id column in outcome 
 
 # Read in subject of outcome 
-outcome_sublist <- read.csv(outcome_sublist_path)
+outcome_sublist <- read.csv(outcome_sublist_path) %>%  # make sure subject id variable name matches subject id column in matrices 
+  select(-X)
 
 # Merge connectivity matrices with available outcome data
-complete_sublist <- merge(goodmatrix_sublist, outcome_sublist, by = "subject_id")
-good_ids <- complete_sublist$subect_id 
+complete_sublist <- merge(goodmatrix_sublist, outcome_sublist, by = "src_subject_id")
+good_ids <- complete_sublist$src_subject_id 
 
 # Pull matrices based on 'complete_sublist' 
 available_ids <- intersect(good_ids, names(connectivity_matrices)) 
@@ -30,5 +37,12 @@ if (length(missing_ids) > 0) {
           paste(missing_ids, collapse = ", "))
 }
 
-# Save filtered matrices 
-write_rds(filtered_matrices,new_rds_path_name)
+
+# Save filtered matrices and outcome file 
+write_rds(filtered_matrices,
+          path = file.path(filtered_matrix_filename, "tp2_mid_alcohol_matrices.rds"))
+
+write.csv(complete_sublist, 
+          path = file.path(filtered_outcome_filename, "tp2_filtered_outcome.csv"))
+
+
